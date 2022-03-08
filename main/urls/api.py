c965013d439217df001eae8320c61c4398db43e8
Axel <axel.mesnard@kedgebs.com>
2022-03-08 17:59:59 +0100

    changed the url pattern

--- main/urls/api.py
+++ main/urls/api.py
@@ -1,17 +1,23 @@
 from django.urls import path, include
+from sales.urls import router as sales_router
 
 from rest_framework import routers
 
 
-router = routers.DefaultRouter(trailing_slash=False)
+router = routers.DefaultRouter()
+router.registry.extend(sales_router.registry)
 
 urlpatterns = [
-    path(
-        "v1/",
-        include(
-            [
-                path("", include(router.urls)),
-            ]
-        ),
-    )
+    path("", include(router.urls)),
 ]
+
+# urlpatterns = [
+#     path(
+#         "v1/",
+#         include(
+#             [
+#                 path("", include(router.urls)),
+#             ]
+#         ),
+#     ),
+# ]
